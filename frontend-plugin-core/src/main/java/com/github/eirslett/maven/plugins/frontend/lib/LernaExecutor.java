415bf9965c6a60e56ab2d60575d0015decfc0bef
Jack <jack@jacks-it-lab.de>
2019-01-31 07:18:30 +0100

    add log

--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/LernaExecutor.java
+++ frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/LernaExecutor.java
@@ -5,17 +5,24 @@ import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class LernaExecutor {
 
     private final ProcessExecutor executor;
+    private final Logger logger;
 
+    
     public LernaExecutor(LernaExecutorConfig config, List<String> arguments,
         Map<String, String> additionalEnvironment) {
+        this.logger = LoggerFactory.getLogger(getClass());
         final String lerna = config.getLernaPath().getAbsolutePath();
         List<String> localPaths = new ArrayList<>();
         localPaths.add(config.getYarnPath().getParent());
         localPaths.add(config.getNodePath().getParent());
+        for(String p:localPaths) {
+            this.logger.info("add {} to PATH",p);
+        }
         executor = new ProcessExecutor(config.getWorkingDirectory(), localPaths,
             Utils.prepend("lerna", arguments), config.getPlatform(), additionalEnvironment);
     }
